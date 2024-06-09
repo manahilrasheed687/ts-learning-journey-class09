9d0cc47677831f7dbbacd45f5fcdd8f3195124eb
Manahil rasheed <manahilrasheed507@gmail.com>
2024-06-09 05:32:31 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env
+// function A function is a code of block designed to perform a particular task
+export {};
+// Basic function:
+//  function printer(){
+//      console.log("Affan")
+//  }
+//  printer()
+// other types
+//      function sum( num1,num2){
+//      let y = num1+num2
+//      console.log(y)
+//  }
+// sum(2,5)
+//  function Bio(Name,Age,profession){
+//     console.log(`My name is ${Name}`);
+//     console.log(`My age is ${Age}`);
+//     console.log(`I am a ${profession}`);
+//  }
+//  Bio('Affan',99,'Teacher')
+// return function 
+//  function sum(num1,num2){
+//  let y = num1+num2;
+//  return y;
+//  }
+//  let z = sum(10,5);
+//  console.log(z)
+// Arrow function 
+//   let sum = (num1,num2)=>{
+//     let y = num1+num2;
+//     console.log(y)
+//     }
+//     let z = sum(10,5);
